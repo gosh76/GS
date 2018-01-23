6a4eb62315c6b622cd786825883a27fb4131abbf
gosh76 <goldiesahni@gmail.com>
2018-01-23 12:06:55 +0530

    23012018

---
+++ List.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 23 10:26:39 2018 by Goldie Sahni
+"""
+l = [1,2,'a']
+type(l)
+t=(1,2,'a')
+type(t)
+d={'a':1,'b':2}
+d['c']=5
+d
+s1={1,2,3}
+type(s1)
+f1 = frozenset({3,8,5,6})
+type(f1)
+b = 'nice'
+a = 'ls'
+my_list=['my','list',a,b]
+my_list
+my_list2 = [[4,5,6,7],[3,4,5,6]]
+my_list[1]
+my_list[-3]
+my_list[-len(my_list)]
+my_list[0]
+my_list[1:3]
+my_list[2:]
+my_list[:3]
+my_list2[1][0]
+my_list2[1][:2]
+my_list2[1][2]>4
+my_list.index(a)
+my_list.count(a)
+my_list.append('!')
+my_list
+my_list.extend('!')
+my_list
+my_list.sort()
+my_list
+my_list.remove('!')
+my_list
+my_list.pop(-1)
+my_list
+del(my_list[0])
+my_list
